a04f4d61b370cca253ba8db36d4135882d3f9141
ThethirdWolf <thethirdwolfcontact1@gmail.com>
2025-05-28 00:34:21 +0200

    Modif mineur

--- src/main/java/vue/HBoxResultat.java
+++ src/main/java/vue/HBoxResultat.java
@@ -4,7 +4,7 @@ import javafx.scene.layout.HBox;
 
 public class HBoxResultat extends HBox {
 
-    public HBoxResultat() {
+    public HBoxResultat(AffichageChemin affichage, VBoxParcours vboxParcours) {
         this.setSpacing(10);
         this.getChildren().addAll(new AffichageChemin(), new VBoxParcours());
     }
