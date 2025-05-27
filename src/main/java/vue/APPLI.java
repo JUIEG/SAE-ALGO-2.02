94ef8393de45d8b603f631c579bb72cee30222a0
ThethirdWolf <thethirdwolfcontact1@gmail.com>
2025-05-27 14:38:27 +0200

    temp

--- src/main/java/vue/APPLI.java
+++ src/main/java/vue/APPLI.java
@@ -1,4 +1,21 @@
 package vue;
 
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class APPLI {
+
+    public static void main(String[] args) {
+        Application.launch(args);}
+
+    private void ouvrirAffichage(Stage stage) {
+        Stage stage1 = new Stage();
+        Scene scene = new Scene();
+
+        stage.setScene(scene);
+        stage.setTitle("APPLI");
+        stage.show();
+    }
 }
